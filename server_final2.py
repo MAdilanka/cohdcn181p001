9c92717100ef437c81ab14ef9efa8fae694de8ea
MAdilanka <40228295+MAdilanka@users.noreply.github.com>
2018-08-22 16:23:51 +0530

    Update server_final2.py

--- server_final2.py
+++ server_final2.py
@@ -1,7 +1,7 @@
 import socket, threading, sys ,os
 try :
-	host='192.168.225.128'
-	port=8080
+	host = sys.argv[1]
+	port = int(sys.argv[2])
 	sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.bind((host, port))
 	sock.listen(1)
@@ -9,41 +9,27 @@ try :
 	conn, client =  sock.accept()
 	print client
 	print "client connected "
-	print "Press 'q' to Exit Program \n \n"
 	
 	def send():
 		while True:
 			server_send=raw_input('\nserver :')
 			conn.send(server_send)
-			if server_send=="q" :
-				sock.close()
-				print "\n.....chat closed......"
-				os._exit(1)
-		
-	def recieve():
-		while True:
-			data = conn.recv(1024) 
-			print '\nClient :' ,data
-			if data == "q":
-				sock.close()
-				print '\n.....chat closed......'
-				os._exit(1)
-			if not data:
-				break
-
-	thread_send = threading.Thread(target = send)
-	thread_send.start()
-
-	thread_recieve = threading.Thread(target = recieve)
-	thread_recieve.start()
-
-except KeyboardInterrupt:
-			print "\nProgram Terminated!"
-			sys.exit(0)
-except socket.error:
-			print "address_already_in_use"
-			sys.exit(0)	
-
 	
-
+	thread_send = threading.Thread(target = send)
+	thread_send.daemon = True
+	thread_send.start()
+except KeyboardInterrupt:
+			sys.exit()
+while True:
+	try:
+		data = conn.recv(1024) 
+		print '\nClient :' ,data	
+		
+	except KeyboardInterrupt:
+			conn.close()
+			sys.exit()
+	else:
+		if not data :
+			break	
+	
 
